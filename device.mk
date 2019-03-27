0da0acd51552f19c9febbc7d023825fe2d21544c
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2019-03-26 21:44:19 -0400

    gauguin: Add Notch Bar Killer overlay
     * This overlay gets rid of the black bar in most apps/games
       and lets the screen fill past the notch.
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -364,6 +364,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Overlays
+PRODUCT_PACKAGES += \
+    NotchBarKiller
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
