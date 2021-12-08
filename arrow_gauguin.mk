8a81646ececd71f12f74226a9c6e5edbd029d421
baolong24 <1667375308@qq.com>
2021-12-08 19:21:54 +0800

    gauguin: Arrow-ify

--- arrow_gauguin.mk
+++ arrow_gauguin.mk
@@ -11,11 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+DEVICE_MAINTAINER := baolong24
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gauguin
+PRODUCT_NAME := arrow_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J17G
