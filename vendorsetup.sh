758a6f016206e92f34672685cdca3834bfc15a30
itsmethekid <pr4j4p4t1.hr1d4y4@gmail.com>
2022-02-12 03:24:49 -0500

    vendorsetup.sh: Removed GcamGo *we already have Gcam go in our dt
    Signed-off-by: itsmethekid <pr4j4p4t1.hr1d4y4@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,2 @@
 git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git device/xiaomi/extra
 git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git hardware/xiaomi
-git clone https://github.com/ArrowOS-Devices/android_packages_apps_GCamGOPrebuilt packages/apps/GCamGOPrebuilt
