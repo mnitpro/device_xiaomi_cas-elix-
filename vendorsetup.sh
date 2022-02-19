920536a4365041ad9241b6551b632717fd4e76da
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-02-19 12:26:23 +0530

    gauguin: Bring-ups For Project Elixir * Add Device and Maintainer props * Add vendorsetup.sh
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+rm -rf device/xiaomi/extras
+rm -rf hardware/xiaomi
+rm -rf vendor/qcom/opensource/fm-commonsys
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git device/xiaomi/extras
+git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git hardware/xiaomi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_gauguin.git kernel/xiaomi/gauguin
+git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_fm-commonsys.git vendor/qcom/opensource/fm-commonsys
