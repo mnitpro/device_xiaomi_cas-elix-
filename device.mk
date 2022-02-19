7e61a00f7c2c930fdcdfabb9c74d5ecf90d9ae56
Lunarixus <Nathan@lunarixus.dev>
2022-02-19 12:54:11 +0530

    [SQUASH]Gauguin: Remove packages at build time Squash of the following:
    violet: remove_packages: Rename RemovePackages for source
    
    violet: remove_packages: Switch to platform certificate
    
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -391,6 +391,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
