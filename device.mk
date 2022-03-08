LineageOS/android_device_xiaomi_gauguin

gauguin: Blacklist FP event
 * After screen unlocked,blacklist FP event to avoid some misbehavior.

Signed-off-by: IsaacChen <tingyi364@gmail.com>
Change-Id: I7403f14a10ba0c91229e968dc4e03a54fb4bb4a3

device.mk
@@ -222,6 +222,11 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc \
+    $(DEVICE_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.default \

mnitpro/device_xiaomi_cas-elix-

device.mk
@@ -229,6 +229,11 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.default \