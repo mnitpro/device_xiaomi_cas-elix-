73015dd41c9b0c3e8ec7296605a21583f85ab864
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-03-11 16:46:58 +0530

    gauguin: De-Commonize IFAAManager
    This reverts commit 70af7c8f4ce971a673a6a08c64e398bd54319302.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 rm -rf device/xiaomi/extras
 rm -rf hardware/xiaomi
 rm -rf vendor/qcom/opensource/fm-commonsys
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git device/xiaomi/extras
 git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git hardware/xiaomi
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_gauguin.git kernel/xiaomi/gauguin
